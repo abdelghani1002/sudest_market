9a9e0388a48a66d55439af914fd3bf425291c3ac
abdelghaniAit <aaittamghart8@gmail.com>
2024-04-04 23:55:35 +0000

    Edit app logo link to

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="https://flowbite.com" class="flex items-center gap-1">
+<a href="{{ route('home') }}" class="flex items-center gap-1">
     <img src="{{ asset('app_logo.png') }}" class="h-8" alt="Flowbite Logo" />
     <span class="self-center text-lg md:text-2xl font-semibold whitespace-nowrap dark:text-white">SudEst
         Market</span>
