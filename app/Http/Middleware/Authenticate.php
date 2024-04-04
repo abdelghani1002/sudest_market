e0b328eff970711501269ba0036727701e720f90
abdelghaniAit <aaittamghart8@gmail.com>
2024-04-04 23:51:43 +0000

    Modify redirect to in auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : redirect()->route('login');
+        return $request->expectsJson() ? null : '/login';
     }
 }
