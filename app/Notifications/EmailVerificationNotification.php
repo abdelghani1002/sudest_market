adbf5d9c29f29718d4b38619404f7f0806575a06
abdelghaniAit <aaittamghart8@gmail.com>
2024-04-04 11:45:33 +0000

    Add ShouldQueue interface to notification classes and create jobs table migration

--- app/Notifications/EmailVerificationNotification.php
+++ app/Notifications/EmailVerificationNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Ichtrojan\Otp\Otp;
 
-class EmailVerificationNotification extends Notification
+class EmailVerificationNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     public $message;
