a9f8bf67db66c73343f4b45384f259beccd9b5ee
abdelghaniAit <aaittamghart8@gmail.com>
2024-04-26 23:41:03 +0100

    Update logo text style and preloader animation

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('home') }}" class="flex items-center gap-1">
     <img src="{{ asset('app_logo.png') }}" class="w-6 md:w-8" alt="app Logo" />
-    <span class="self-center md:text-2xl font-semibold whitespace-nowrap dark:text-white">SudEst
+    <span id="logo_text" class="self-center md:text-2xl font-bold whitespace-nowrap dark:text-white">SudEst
         Market
     </span>
 </a>
